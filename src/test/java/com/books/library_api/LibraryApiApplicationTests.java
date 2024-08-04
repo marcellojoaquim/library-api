7883d7cfd1df759a17edc0f96a603955d7d312ce
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-08-04 18:35:52 -0300

    initial library api project

---
+++ src/test/java/com/books/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
