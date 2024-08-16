44e139f9f7e35e78ecb831f03ec96915a0af8b58
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-08-15 23:24:51 -0300

    Implement serializable for entities

--- src/main/java/com/books/libraryapi/model/entity/Book.java
+++ src/main/java/com/books/libraryapi/model/entity/Book.java
@@ -6,13 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table
-public class Book {
+public class Book implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/books/libraryapi/model/entity/Loan.java
+++ src/main/java/com/books/libraryapi/model/entity/Loan.java
@@ -6,13 +6,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Loan {
+public class Loan implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Long id;
     private String customer;
